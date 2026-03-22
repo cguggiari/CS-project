e8389f45b3928433da082fb28cea8bfc02468d3a
cguggiari <clarissa.guggiari@student.unisg.ch>
2026-03-22 10:58:58 +0100

    love

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.write('Yeay, we connected everything')
 st.write('celine is a ABBUZZICONA')
 st.write('love clarissa')
-st.slider(label:str, min_value:ananas = 0, max_value: banana = 10)
+st.slider(label:str,min_value:ananas = 0,max_value: banana = 10)
