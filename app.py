42435e822794c5b399ee68fc663640efcfe405c1
cguggiari <clarissa.guggiari@student.unisg.ch>
2026-05-01 15:26:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.write('Yeay, we connected everything')
 st.write('celine is a ABBUZZICONA')
 st.write('love clarissa')
-st.slider(label:str,min_value:ananas = 0,max_value: banana = 10)
 import json
 import requests
 from datetime import datetime, timedelta
